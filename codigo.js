a0333ca5251e03cffcb692130963e3111d64111d
naomipoclava2021 <milagrito1dediciembre@gmail.com>
2022-01-18 17:40:24 -0300

    Agregando imagen de llave

--- codigo.js
+++ codigo.js
@@ -2,22 +2,25 @@ const contenedor = document.querySelector (".flex-container");
 let contador = 0;
 function crearLlave(nombre,modelo,precio) {
     contador++;
-    img = "<img src='llave.png'>";
+    img = "<img class='llave-img' src='llave.png'>";
     nombre =`<h2>${nombre}</h2>`;
     modelo =`<h3>${modelo}</h3>`;
     precio =`<p>Precio: <b>$${precio}</b></p>`;
     return [img,nombre,modelo,precio];   
 }
 
+let documentFragment = document.createDocumentFragment();
 
-for (let  = 0;  < 20 ; i++) {
-    let modeloRandom = Math.random()*10000;
-    let precioRandom = Math.random()*10+30;
+for (var i = 1; i <= 20 ; i++) {
+    let modeloRandom = Math.round(Math.random()*10000);
+    let precioRandom = Math.round(Math.random()*10+30);
     let llave = crearLlave(`llave ${i}`, `modelo ${modeloRandom}`,precioRandom);
     let div = document.createElement("DIV");
     div.classList.add(`iten-${i}`,'flex-item');
-    div.innerHTML = llave;
-    contenedor.innerHTML += llave;
+    div.innerHTML = llave[0] + llave[1] + llave[2] + llave[3];
+    documentFragment.appendChild(div)
 }
+
+contenedor.appendChild(documentFragment);
 
 
